1de2e0abc8704f6f6537f43695c52cee18d0c8e0
Allan Gregory <alngregory@gmail.com>
2020-01-05 20:00:13 -0300

    Inclusão do contador de vitórias

--- app/src/main/java/com/example/mtglifecounter/MainActivity.java
+++ app/src/main/java/com/example/mtglifecounter/MainActivity.java
@@ -9,24 +9,38 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView contador_txt;
-    private Button botao_menos_btn;
-    private Button botao_resetar_btn;
-    private Button botao_mais_btn;
-    private int contador = 20;
+    private TextView contadorVida_txt;
+    private Button botaoMenosVida_btn;
+    private Button botaoResetarVida_btn;
+    private Button botaoMaisVida_btn;
+    private TextView contadorVitoria_txt;
+    private Button botaoMenosVitoria_btn;
+    private Button botaoResetarVitoria_btn;
+    private Button botaoMaisVitoria_btn;
+    private int contadorVida = 20;
+    private int contadorVitoria = 0;
 
     private View.OnClickListener clickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
             switch (view.getId()) {
-                case R.id.botaoMenos_btn:
-                    contadorMenos();
+                case R.id.botaoMenosVida_btn:
+                    contadorMenosVida();
                     break;
-                case R.id.botaoMais_btn:
-                    contadorMais();
+                case R.id.botaoMaisVida_btn:
+                    contadorMaisVida();
                     break;
-                case R.id.botaoResetar_btn:
-                    iniciaContador();
+                case R.id.botaoResetarVida_btn:
+                    iniciaContadorVida();
+                    break;
+                case R.id.botaoMenosVitoria_btn:
+                    contadorMenosVitoria();
+                    break;
+                case R.id.botaoMaisVitoria_btn:
+                    contadorMaisVitoria();
+                    break;
+                case R.id.botaoResetarVitoria_btn:
+                    iniciaContadorVitoria();
                     break;
             }
         }
@@ -36,27 +50,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        contador_txt = (TextView) findViewById(R.id.contador_txt);
-        botao_menos_btn = (Button) findViewById(R.id.botaoMenos_btn);
-        botao_menos_btn.setOnClickListener(clickListener);
-        botao_resetar_btn = (Button) findViewById(R.id.botaoResetar_btn);
-        botao_resetar_btn.setOnClickListener(clickListener);
-        botao_mais_btn = (Button) findViewById(R.id.botaoMais_btn);
-        botao_mais_btn.setOnClickListener(clickListener);
+        contadorVida_txt = (TextView) findViewById(R.id.contadorVida_txt);
+        botaoMenosVida_btn = (Button) findViewById(R.id.botaoMenosVida_btn);
+        botaoMenosVida_btn.setOnClickListener(clickListener);
+        botaoResetarVida_btn = (Button) findViewById(R.id.botaoResetarVida_btn);
+        botaoResetarVida_btn.setOnClickListener(clickListener);
+        botaoMaisVida_btn = (Button) findViewById(R.id.botaoMaisVida_btn);
+        botaoMaisVida_btn.setOnClickListener(clickListener);
+
+        contadorVitoria_txt = (TextView) findViewById(R.id.contadorVitoria_txt);
+        botaoMenosVitoria_btn = (Button) findViewById(R.id.botaoMenosVitoria_btn);
+        botaoMenosVitoria_btn.setOnClickListener(clickListener);
+        botaoResetarVitoria_btn = (Button) findViewById(R.id.botaoResetarVitoria_btn);
+        botaoResetarVitoria_btn.setOnClickListener(clickListener);
+        botaoMaisVitoria_btn = (Button) findViewById(R.id.botaoMaisVitoria_btn);
+        botaoMaisVitoria_btn.setOnClickListener(clickListener);
     }
 
-    private void iniciaContador() {
-        contador = 20;
-        contador_txt.setText(contador + "");
+    private void iniciaContadorVida() {
+        contadorVida = 20;
+        contadorVida_txt.setText(contadorVida + "");
     }
 
-    private void contadorMais() {
-        contador++;
-        contador_txt.setText(contador + "");
+    private void contadorMaisVida() {
+        contadorVida++;
+        contadorVida_txt.setText(contadorVida + "");
     }
 
-    private void contadorMenos() {
-        contador--;
-        contador_txt.setText(contador + "");
+    private void contadorMenosVida() {
+        contadorVida--;
+        contadorVida_txt.setText(contadorVida + "");
+    }
+
+    private void iniciaContadorVitoria() {
+        contadorVitoria = 0;
+        contadorVitoria_txt.setText(contadorVitoria + "");
+    }
+
+    private void contadorMaisVitoria() {
+        contadorVitoria++;
+        contadorVitoria_txt.setText(contadorVitoria + "");
+    }
+
+    private void contadorMenosVitoria() {
+        contadorVitoria--;
+        contadorVitoria_txt.setText(contadorVitoria + "");
     }
 }
