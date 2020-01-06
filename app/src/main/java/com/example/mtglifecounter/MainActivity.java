429038048dfb7884baa8bbd9d853fc2ab85ca101
Allan Gregory <alngregory@gmail.com>
2020-01-05 22:39:35 -0300

    Inclusão do contador de vida e vitórias para o segundo jogador

--- app/src/main/java/com/example/mtglifecounter/MainActivity.java
+++ app/src/main/java/com/example/mtglifecounter/MainActivity.java
@@ -20,6 +20,17 @@ public class MainActivity extends AppCompatActivity {
     private int contadorVida = 20;
     private int contadorVitoria = 0;
 
+    private TextView contadorVida_jogador2_txt;
+    private Button botaoMenosVida_jogador2_btn;
+    private Button botaoResetarVida_jogador2_btn;
+    private Button botaoMaisVida_jogador2_btn;
+    private TextView contadorVitoria_jogador2_txt;
+    private Button botaoMenosVitoria_jogador2_btn;
+    private Button botaoResetarVitoria_jogador2_btn;
+    private Button botaoMaisVitoria_jogador2_btn;
+    private int contadorVida_jogador2 = 20;
+    private int contadorVitoria_jogador2 = 0;
+
     private View.OnClickListener clickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
@@ -42,6 +53,24 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.botaoResetarVitoria_btn:
                     iniciaContadorVitoria();
                     break;
+                case R.id.botaoMenosVida_jogador2_btn:
+                    contadorMenosVida_jogador2();
+                    break;
+                case R.id.botaoMaisVida_jogador2_btn:
+                    contadorMaisVida_jogador2();
+                    break;
+                case R.id.botaoResetarVida_jogador2_btn:
+                    iniciaContadorVida_jogador2();
+                    break;
+                case R.id.botaoMenosVitoria_jogador2_btn:
+                    contadorMenosVitoria_jogador2();
+                    break;
+                case R.id.botaoMaisVitoria_jogador2_btn:
+                    contadorMaisVitoria_jogador2();
+                    break;
+                case R.id.botaoResetarVitoria_jogador2_btn:
+                    iniciaContadorVitoria_jogador2();
+                    break;
             }
         }
     };
@@ -57,7 +86,6 @@ public class MainActivity extends AppCompatActivity {
         botaoResetarVida_btn.setOnClickListener(clickListener);
         botaoMaisVida_btn = (Button) findViewById(R.id.botaoMaisVida_btn);
         botaoMaisVida_btn.setOnClickListener(clickListener);
-
         contadorVitoria_txt = (TextView) findViewById(R.id.contadorVitoria_txt);
         botaoMenosVitoria_btn = (Button) findViewById(R.id.botaoMenosVitoria_btn);
         botaoMenosVitoria_btn.setOnClickListener(clickListener);
@@ -65,6 +93,21 @@ public class MainActivity extends AppCompatActivity {
         botaoResetarVitoria_btn.setOnClickListener(clickListener);
         botaoMaisVitoria_btn = (Button) findViewById(R.id.botaoMaisVitoria_btn);
         botaoMaisVitoria_btn.setOnClickListener(clickListener);
+
+        contadorVida_jogador2_txt = (TextView) findViewById(R.id.contadorVida_jogador2_txt);
+        botaoMenosVida_jogador2_btn = (Button) findViewById(R.id.botaoMenosVida_jogador2_btn);
+        botaoMenosVida_jogador2_btn.setOnClickListener(clickListener);
+        botaoResetarVida_jogador2_btn = (Button) findViewById(R.id.botaoResetarVida_jogador2_btn);
+        botaoResetarVida_jogador2_btn.setOnClickListener(clickListener);
+        botaoMaisVida_jogador2_btn = (Button) findViewById(R.id.botaoMaisVida_jogador2_btn);
+        botaoMaisVida_jogador2_btn.setOnClickListener(clickListener);
+        contadorVitoria_jogador2_txt = (TextView) findViewById(R.id.contadorVitoria_jogador2_txt);
+        botaoMenosVitoria_jogador2_btn = (Button) findViewById(R.id.botaoMenosVitoria_jogador2_btn);
+        botaoMenosVitoria_jogador2_btn.setOnClickListener(clickListener);
+        botaoResetarVitoria_jogador2_btn = (Button) findViewById(R.id.botaoResetarVitoria_jogador2_btn);
+        botaoResetarVitoria_jogador2_btn.setOnClickListener(clickListener);
+        botaoMaisVitoria_jogador2_btn = (Button) findViewById(R.id.botaoMaisVitoria_jogador2_btn);
+        botaoMaisVitoria_jogador2_btn.setOnClickListener(clickListener);
     }
 
     private void iniciaContadorVida() {
@@ -95,5 +138,35 @@ public class MainActivity extends AppCompatActivity {
     private void contadorMenosVitoria() {
         contadorVitoria--;
         contadorVitoria_txt.setText(contadorVitoria + "");
+    }
+
+    private void iniciaContadorVida_jogador2() {
+        contadorVida_jogador2 = 20;
+        contadorVida_jogador2_txt.setText(contadorVida_jogador2 + "");
+    }
+
+    private void contadorMaisVida_jogador2() {
+        contadorVida_jogador2++;
+        contadorVida_jogador2_txt.setText(contadorVida_jogador2 + "");
+    }
+
+    private void contadorMenosVida_jogador2() {
+        contadorVida_jogador2--;
+        contadorVida_jogador2_txt.setText(contadorVida_jogador2 + "");
+    }
+
+    private void iniciaContadorVitoria_jogador2() {
+        contadorVitoria_jogador2 = 0;
+        contadorVitoria_jogador2_txt.setText(contadorVitoria_jogador2 + "");
+    }
+
+    private void contadorMaisVitoria_jogador2() {
+        contadorVitoria_jogador2++;
+        contadorVitoria_jogador2_txt.setText(contadorVitoria_jogador2 + "");
+    }
+
+    private void contadorMenosVitoria_jogador2() {
+        contadorVitoria_jogador2--;
+        contadorVitoria_jogador2_txt.setText(contadorVitoria_jogador2 + "");
     }
 }
